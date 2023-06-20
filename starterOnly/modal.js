6800334627a6409afda0288b5600d5f513edd495
Jalal El Fanid <j.elfanid@gmail.com>
2023-06-20 20:35:13 +0200

    Projet 4 OC Créez une landing page avec JS

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,16 +8,202 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const modalbg = document.querySelector(".bground")
+const modalsub =document.querySelector(".bsubmitted")
+const modalBtn = document.querySelectorAll(".modal-btn")
+const formData = document.querySelectorAll(".formData") 
+const btnClose = document.querySelector("#btn-close")
+const btnSubmit = document.querySelector(".btn-submit")
+
+// form elements
+const firstNameInput = document.querySelector("#first-name")
+const lastNameInput = document.querySelector("#last-name")
+const emailInput = document.querySelector("#your-email")
+const dateInput = document.querySelector("#your-birthdate")
+const quantiteInput =document.querySelector("#quantity")
+const locationRadio = document.getElementsByName("location")
+const conditionCheck = document.querySelector("#checkbox1");
+
+const form = document.getElementById("form") // pas besoin de # avec getElementById
+
+/*********************************************************/
+// const erreurFrame = document.querySelector(".formData")
+/********************************************************/
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal))
+
+
+
+form.addEventListener("submit",function(e){
+  e.preventDefault()
+  const prenomValide = checkPrenom()
+  const nomValide = checkNom()
+  const mailValide =checkMail()
+  const dateValide = checkDate()
+  const quantiteValide = checkNumber()
+  const locationValide = checkOption()
+  const conditionValide = checkCondition()
+
+  
+  if (!prenomValide || !nomValide  || !mailValide || !dateValide  || !quantiteValide || !locationValide )
+  {
+    console.log("Veuillez saisir tous les champs correctement")
+  }
+  else if (!conditionValide){
+    console.log("Veuillez accepter les conditions")
+  }
+  else {
+    // lancer la feuille de validation
+    launchModal2()
+    console.log("C'est OK !")
+    
+  }
+})
+    
+  
+
+
+btnClose.addEventListener("click",closeModal)
+
+
+
+// control first name field
+firstNameInput.addEventListener("input",checkPrenom)
+ 
+function checkPrenom(){
+  const prenom = firstNameInput.value
+  if (prenom.length<2 ||  prenom == null){
+    firstNameInput.focus()
+    document.getElementById("erreurPrenom").style.visibility = 'visible'
+    firstNameInput.setAttribute("data-error-visible", "true")
+    return false
+  }
+  else{
+    document.getElementById("erreurPrenom").style.visibility = 'hidden'
+    firstNameInput.setAttribute("data-error-visible", "false")
+    return true
+  }
+}
+
+// control last name field
+lastNameInput.addEventListener("input",checkNom)
+
+function checkNom(){
+  const nom = lastNameInput.value
+  if (nom.length<2 ||  nom == null){
+    lastNameInput.focus()
+    document.getElementById("erreurNom").style.visibility = 'visible'
+    lastNameInput.setAttribute("data-error-visible", "true")
+    return false
+  }
+  else{
+    document.getElementById("erreurNom").style.visibility = 'hidden'
+    lastNameInput.setAttribute("data-error-visible", "false")
+    return true
+  }
+}
+
+// control email field
+emailInput.addEventListener("input",checkMail)
+
+function checkMail(){
+  const adressMail = emailInput.value;
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(adressMail))
+    {
+      document.getElementById("erreurMail").style.visibility = 'hidden'
+      emailInput.setAttribute("data-error-visible", "false")
+      return true
+    }
+  emailInput.focus()
+  document.getElementById("erreurMail").style.visibility = 'visible'
+  emailInput.setAttribute("data-error-visible", "true")
+  return false
+}
+
+//dateInput.addEventListener("focusout",checkDate)
+//control date field
+function checkDate(){
+  const birthdate = dateInput.value;
+  if (birthdate != "")
+    {
+      document.getElementById("erreurDate").style.visibility = 'hidden'
+      dateInput.setAttribute("data-error-visible", "false")
+      return true
+    }
+  dateInput.focus()
+  document.getElementById("erreurDate").style.visibility = 'visible'
+  dateInput.setAttribute("data-error-visible", "true")
+  return false
+}
+
+//quantiteInput.addEventListener("focusout",checkNumber)
+//fonction checkNumber qui controle qu'on a bien un nombre saisi
+function checkNumber(){
+  const quantite = quantiteInput.value;
+  if (quantite !='')
+    {
+      document.getElementById("erreurNumber").style.visibility = 'hidden'
+      quantiteInput.setAttribute("data-error-visible", "false")
+      return true
+    }
+  quantiteInput.focus()
+  document.getElementById("erreurNumber").style.visibility = 'visible'
+  quantiteInput.setAttribute("data-error-visible", "true")
+  return false
+}
+
+
+// fonction checkOption qui controle qu'un option radio a bien été choisi
+
+function checkOption(){
+  
+  let villeChoisie = false
+  for( i = 0; i < locationRadio.length; i++){
+   if(locationRadio[i].checked){
+    villeChoisie = true
+   }
+  }
+
+  if (villeChoisie)
+  {
+    document.getElementById("erreurOption").style.visibility = 'hidden'
+    return true 
+    }
+  document.getElementById("erreurOption").style.visibility = 'visible'
+  return false
+}
+
+
+// fonction checkCondition qui vérifie que les conditions ont été acceptées
+function checkCondition(){
+if (conditionCheck.checked)
+  {
+  document.getElementById("erreurCheck").style.visibility = 'hidden'
+  return true 
+  }
+document.getElementById("erreurCheck").style.visibility = 'visible'
+return false
+}
+
+
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalbg.style.display = "block"
+}
+
+
+function launchModal2() {
+  modalsub.style.display = "block";
+  console.log("C'est OK !")
+  closeModal()
+ 
+}
+
+//close the modal form
+function closeModal(){
+  modalbg.style.display="none"
 }
 
 
